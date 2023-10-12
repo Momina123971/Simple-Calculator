b199983938d26b11866282023edb95cb72425e73
Momina Mehboob <mehboobmomina2@gmail.com>
2023-10-12 11:18:11 +0500

    Added main file

---
+++ main.py
@@ -0,0 +1,9 @@
+from addition import add
+from multiplication import multiply
+from subtraction import subtract
+if __name__=="__main__":
+ A=10
+ B=5
+ print(add(A,B))
+ print(multiply(A,B))
+ print(subtract(A,B))
